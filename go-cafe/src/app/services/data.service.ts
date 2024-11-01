94193d3461f58bf6bf083f846d44b18207c0735e
MohamedNsiri <118359705+MohamedNsiri@users.noreply.github.com>
2024-11-01 22:43:20 +0100

    c10

--- go-cafe/src/app/services/data.service.ts
+++ go-cafe/src/app/services/data.service.ts
@@ -8,7 +8,7 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class DataService {
   private platformId: Object;
-  private apiUrl: string = 'https://full-stack-go-cafe-application-production.up.railway.app:8080/api';
+  private apiUrl: string = 'https://full-stack-go-cafe-application-production.up.railway.app/api';
 
   constructor(private http: HttpClient, @Inject(PLATFORM_ID) platformId: Object) {
     this.platformId = platformId;
