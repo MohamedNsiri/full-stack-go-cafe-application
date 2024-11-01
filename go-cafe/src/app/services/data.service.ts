50c37ff4b760b06991cf3d47199aa5d1e7ed9e25
MohamedNsiri <118359705+MohamedNsiri@users.noreply.github.com>
2024-11-01 13:41:31 +0100

    commit7

--- go-cafe/src/app/services/data.service.ts
+++ go-cafe/src/app/services/data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams, HttpHeaders  } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
@@ -8,6 +8,7 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class DataService {
   private platformId: Object;
+  private apiUrl: string = 'https://full-stack-go-cafe-application-production.up.railway.app:9000/api';
 
   constructor(private http: HttpClient, @Inject(PLATFORM_ID) platformId: Object) {
     this.platformId = platformId;
@@ -16,7 +17,7 @@ export class DataService {
   users: any = [];
 
   create_account(userData: any): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:8000/api/create_account', userData);
+    return this.http.post<any>(`${this.apiUrl}/create_account`, userData);
   }
 
   email_verification(email: string, email_verification_token: string): Observable<any> {
@@ -24,15 +25,15 @@ export class DataService {
       .set('email', email)
       .set('email_verification_token', email_verification_token);
 
-    return this.http.get<any>('http://127.0.0.1:8000/api/email_verification', { params });
+    return this.http.get<any>(`${this.apiUrl}/email_verification`, { params });
   }
 
   resend_email_verification(email: string): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:8000/api/resend_email_verification', { email });
+    return this.http.post<any>(`${this.apiUrl}/resend_email_verification`, { email });
   }
 
   login(data: any): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:8000/api/login', data);
+    return this.http.post<any>(`${this.apiUrl}/login`, data);
   }
 
   storeToken(token: string) {
@@ -54,8 +55,8 @@ export class DataService {
     }
   }
 
-  deleteAccount(){
-    return this.http.delete<any>('http://127.0.0.1:8000/api/delete_account');
+  deleteAccount(): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/delete_account`);
   }
 
   isLoggedIn() {
@@ -63,48 +64,37 @@ export class DataService {
   }
 
   getUserInfo(): Observable<any> {
-    return this.http.get<any>('http://127.0.0.1:8000/api/user');
+    return this.http.get<any>(`${this.apiUrl}/user`);
   }
 
-  updateName(name: string, id: string){
-    return this.http.post<any>(`http://127.0.0.1:8000/api/update_name/${id}`, {name: name});
+  updateName(name: string, id: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/update_name/${id}`, { name });
   }
 
-  updatePassword(data: any){
-    return this.http.post<any>(`http://127.0.0.1:8000/api/update_password`, data);
+  updatePassword(data: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/update_password`, data);
   }
 
-  forgetPassword(data: any){
-    return this.http.post<any>(`http://127.0.0.1:8000/api/forget_password`, data)
+  forgetPassword(data: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/forget_password`, data);
   }
 
-  resetPassword(password: string, email: string, token: string){
-    const body = {
-      newPassword: password,
-      email: email,
-      token: token,
-    };
-    return this.http.post<any>(`http://127.0.0.1:8000/api/reset_password`, body)
+  resetPassword(password: string, email: string, token: string): Observable<any> {
+    const body = { newPassword: password, email, token };
+    return this.http.post<any>(`${this.apiUrl}/reset_password`, body);
   }
 
-  submitFeedback(email: string, message_body: string, rating: number){
-    const body = {
-      email: email,
-      message_body: message_body,
-      rating: rating,
-    };
-    return this.http.post<any>(`http://127.0.0.1:8000/api/submit_feedback`, body);
+  submitFeedback(email: string, message_body: string, rating: number): Observable<any> {
+    const body = { email, message_body, rating };
+    return this.http.post<any>(`${this.apiUrl}/submit_feedback`, body);
   }
 
-  showFeedbacks(){
-    return this.http.get<any>(`http://127.0.0.1:8000/api/get_feedbacks`);
+  showFeedbacks(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/get_feedbacks`);
   }
 
-  sendEmail(email: string, email_body:string){
-    const body = {
-      email: email,
-      email_body: email_body
-    }
-    return this.http.post<any>(`http://127.0.0.1:8000/api/send_email`, body);
+  sendEmail(email: string, email_body: string): Observable<any> {
+    const body = { email, email_body };
+    return this.http.post<any>(`${this.apiUrl}/send_email`, body);
   }
 }
